7ed45e353c9cf7fc25b315594981dd65d0218f37
Braullio Gonacalves <braulliovg@gmail.com>
2023-05-28 22:28:09 -0300

    feat: create open_ai_service

---
+++ lib/services/open_ai_service.rb
@@ -0,0 +1,17 @@
+require 'openai'
+require 'dotenv'
+Dotenv.load
+
+class OpenAiService
+  def initialize
+    @openai_client = OpenAI::Client.new(access_token: ENV['OPEN_AI_KEY'])
+  end
+
+  def call
+    @openai_client.chat(
+      parameters: {
+        model: "gpt-3.5-turbo",
+        messages: [{ role: 'user', content: "Qual o nome do Pelé?"}],
+      })
+  end
+end
